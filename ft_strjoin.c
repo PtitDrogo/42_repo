2365f883ac05d94d8133ee03f95f7de2bf5c5ac7
Theo Freydiere <tfreydie@z3r7p1.42lyon.fr>
2023-11-26 19:52:30 +0100

    changement apres avoir push, ptet des trucs oublies

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: tfreydie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 11:56:36 by tfreydie          #+#    #+#             */
-/*   Updated: 2023/11/15 13:52:39 by tfreydie         ###   ########.fr       */
+/*   Updated: 2023/11/26 19:49:59 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,23 +17,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*joined;
 	size_t	i;
 	size_t	j;
+	size_t	s1_len;
+	size_t	s2_len;
 
 	if (!s1 || !s2)
 		return (NULL);
-	i = 0;
+	
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	i = -1;
 	j = 0;
-	joined = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	joined = malloc(sizeof(char) * (s1_len + s2_len + 1));
 	if (!joined)
 		return (NULL);
-	while (i < ft_strlen(s1))
-	{
+	while (++i < s1_len)
 		joined[i] = s1[i];
-		i++;
-	}
-	while (j < ft_strlen(s2))
-	{
+	while (j < s2_len)
 		joined[i++] = s2[j++];
-	}
 	joined[i] = '\0';
 	return (joined);
 }
@@ -41,7 +41,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 int	main(void)
 {
 	char	*s1 = "abc";
-	char	*s2 = "";
+	char	*s2 = "defg";
 	char	*result = ft_strjoin(s1, s2);
 	#include <stdio.h>
 	printf("%s\n", result);
