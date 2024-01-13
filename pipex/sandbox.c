f3d4b7d2543f178e9f193938ddbc87328a8d462e
Theo Freydiere <tfreydie@z3r12p1.42lyon.fr>
2024-01-13 19:05:45 +0100

    la vie de ma mere je me casse la tete pour rien mais je pige pas le pattern123123

--- pipex/sandbox.c
+++ pipex/sandbox.c
@@ -1,33 +1,77 @@
+// #include "pipex.h"
+// #include <stdio.h>
+
+
+// int main(int    argc, char *argv[])
+// {
+//     int id;
+//     int processes = argc - 1;
+//     int currentindex_process;
+//     int i = 0
+    
+//     while (i > processes)
+//     {
+//         id = fork();
+
+//         if (id == 0)
+//         {
+//             currentindex_process = argc - processes; 
+//             execlp(argv[currentindex_process], argv[currentindex_process], NULL);
+//             perror("execlp"); 
+//             exit(EXIT_FAILURE);
+//         }
+//         else 
+//         {
+//             processes--;
+//             waitpid(id, NULL, 0);
+//             printf("Parent waited for any child process to finish\n");
+//         }
+//     }
+//     printf("success !\n");
+//     exit(0);
+//     return (0);
+// }
+
 #include "pipex.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-
-int main(int    argc, char *argv[])
-{
-    int id;
+int main(int argc, char *argv[]) {
     int processes = argc - 1;
     int currentindex_process;
-    int i = 0
-    
-    while (i > processes)
-    {
-        id = fork();
+    int i = 1;
 
-        if (id == 0)
-        {
-            currentindex_process = argc - processes; 
-            execlp(argv[currentindex_process], argv[currentindex_process], NULL);
-            perror("execlp"); 
+    while(i < argc) 
+    {
+        int id = fork();
+
+        if (id == -1) {
+            perror("fork");
             exit(EXIT_FAILURE);
         }
-        else 
-        {
-            processes--;
-            waitpid(id, NULL, 0);
-            printf("Parent waited for any child process to finish\n");
+        if (id == 0) {
+            execlp(argv[i], argv[i], NULL);
+            perror("execlp");
+            exit(EXIT_FAILURE);
         }
+        i++;
     }
-    printf("success !\n");
-    exit(0);
-    return (0);
+
+    while (processes > 0) {
+        int status;
+        pid_t child_pid = wait(&status);
+        
+        if (child_pid == -1) {
+            perror("wait");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Child process %d finished\n", child_pid);
+        processes--;
+    }
+
+    printf("All child processes have finished.\n");
+    return 0;
 }
