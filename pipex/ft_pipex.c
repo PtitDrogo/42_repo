6b806ba90d080b8333475610227e41b104b8e1e2
Theo Freydiere <tfreydie@z2r4p8.42lyon.fr>
2024-01-24 20:06:33 +0100

    ca avance wllah

--- pipex/ft_pipex.c
+++ pipex/ft_pipex.c
@@ -6,7 +6,7 @@
 /*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 18:40:58 by tfreydie          #+#    #+#             */
-/*   Updated: 2024/01/24 19:42:08 by tfreydie         ###   ########.fr       */
+/*   Updated: 2024/01/24 20:05:26 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void           free_all_fds(int index, int **fds);
 void            close_all_pipes(t_command_line  *cmd_line);
 void            secure_dup2(int old_fd, int new_fd);
 void            init_pipes(t_command_line  *cmd_line);
+void            child_process(int argc, char *argv[], t_command_line  *cmd_line, int i);
+void            process_behavior(int argc, char *argv[], t_command_line  *cmd_line, int position);
 
 
 void	       ft_putnbr_fd(int n, int fd)
@@ -114,84 +116,29 @@ void    free_all_fds(int index, int **fds)
 int main(int argc, char *argv[])
 {
     t_command_line cmd_line;
-    int i = 0;
+    int i;
     if (argc < 5)
         return (perror("Not enough arguments"), 1);
     init_all(argc, argv, &cmd_line);
     if (cmd_line.is_err)
         return(perror("error during initialization"),  1);
     init_pipes(&cmd_line);
-    i = 2; //probably should rename this so i wont be lost later
-    while (i < argc - 1)
-    {
-        cmd_line.current_process = i - 2;
-        cmd_line.child_ids[cmd_line.current_process] = fork();
-        if (cmd_line.child_ids[cmd_line.current_process] == -1)
-        {
-            perror("Error opening file");
-            exit(EXIT_FAILURE);
-        }
-        if (cmd_line.child_ids[cmd_line.current_process] == 0)
-        {
-            int j = 0;
-            if (i == 2)
-            {    
-                cmd_line.infile = open(argv[1], O_WRONLY | O_CREAT | O_APPEND, 0777);
-                secure_dup2(cmd_line.infile, STDIN_FILENO);
-                secure_dup2(cmd_line.fd[cmd_line.current_pipe][1], STDOUT_FILENO);
-            }
-            else if ((i + 1) == argc - 1)
-            {    
-                cmd_line.outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
-                secure_dup2(cmd_line.outfile, STDOUT_FILENO);
-                secure_dup2(cmd_line.fd[cmd_line.current_pipe][0], STDIN_FILENO);
-            }
-            else
-            {
-                secure_dup2(cmd_line.fd[cmd_line.current_pipe + 1][1], STDOUT_FILENO);
-                secure_dup2(cmd_line.fd[cmd_line.current_pipe][0], STDIN_FILENO);
-            }
-            close_all_pipes(&cmd_line);
-            if (cmd_line.is_err)
-            {
-                perror("Error closing file descriptor in child");
-                exit(EXIT_FAILURE);
-            }
-            execlp(argv[i], argv[i], NULL);
-            perror("Error executing child process");
-            exit(EXIT_FAILURE);
-        }
-        if (cmd_line.child_ids[cmd_line.current_process] > 0 && i != 2)
-            cmd_line.current_pipe++;           
-        i++;
-    }
-    i = 0;
-    // ft_putnbr_fd(cmd_line.child_ids[0], 2);
-    // write(2, "is child id\n", 12);
-    // write(2, "i et argc sont egaux a ", 23);
-    // ft_putnbr_fd(i, 2);
-    // write(2, " - ", 3);
-    // ft_putnbr_fd(argc, 2);
-    // write(2, "\n", 1);
+    i = 1; 
+    while (++i < argc - 1)       
+        child_process(argc,argv, &cmd_line, i);
     close_all_pipes(&cmd_line);
-    while (i < (argc - 3))
+    i = -1;
+    while (++i < (argc - 3))
     {
-        // write(2, "JVAI ATTENDre je suis le child : ", 33);
-        // ft_putnbr_fd(cmd_line.child_ids[i], 2);
-        // write(2, "\n", 1);
         if (waitpid(cmd_line.child_ids[i], &cmd_line.status, 0) == -1)
         {
             perror("error waiting for children");
             exit(EXIT_FAILURE);
         }
-        // write(2, "j'ai attendu ", 13);
-        // ft_putnbr_fd(cmd_line.child_ids[i], 2);
-        // write(2, "\n", 1);
-        i++;
     }
     if (cmd_line.is_err)
         return (perror("error happened"), 1); //TODO - error recognition
-    free_all_fds(cmd_line.pipes, cmd_line.fd); //using pipes here might be wrong
+    free_all_fds(cmd_line.pipes, cmd_line.fd); 
     free(cmd_line.child_ids); 
     return (0);
 }
@@ -235,7 +182,51 @@ void secure_dup2(int old_fd, int new_fd)
     return ;
 }
 
-void    child_process(t_command_line  *cmd_line)
+void    child_process(int argc, char *argv[], t_command_line  *cmd_line, int i)
 {
-    
+    cmd_line->current_process = i - 2;
+    cmd_line->child_ids[cmd_line->current_process] = fork();
+    if (cmd_line->child_ids[cmd_line->current_process] == -1)
+    {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+    if (cmd_line->child_ids[cmd_line->current_process] == 0)
+    {
+        process_behavior(argc, argv, cmd_line, i);
+        close_all_pipes(cmd_line);
+        if (cmd_line->is_err)
+        {
+            perror("Error closing file descriptor in child");
+            exit(EXIT_FAILURE);
+        }
+        execlp(argv[i], argv[i], NULL);
+        perror("Error executing child process");
+        exit(EXIT_FAILURE);
+    }
+    if (cmd_line->child_ids[cmd_line->current_process] > 0 && i != 2)
+        cmd_line->current_pipe++;           
+    return ;
+}
+
+void    process_behavior(int argc, char *argv[], t_command_line  *cmd_line, int position)
+{
+    if (position == 2)
+    {    
+        cmd_line->infile = open(argv[1], O_WRONLY | O_CREAT | O_APPEND, 0777);
+        secure_dup2(cmd_line->infile, STDIN_FILENO);
+        secure_dup2(cmd_line->fd[cmd_line->current_pipe][1], STDOUT_FILENO);
+    }
+    else if ((position + 1) == argc - 1)
+    {    
+        cmd_line->outfile = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+        secure_dup2(cmd_line->outfile, STDOUT_FILENO);
+        secure_dup2(cmd_line->fd[cmd_line->current_pipe][0], STDIN_FILENO);
+    }
+    else
+    {
+        secure_dup2(cmd_line->fd[cmd_line->current_pipe + 1][1], STDOUT_FILENO);
+        secure_dup2(cmd_line->fd[cmd_line->current_pipe][0], STDIN_FILENO);
+    }
+    return ;
 }
