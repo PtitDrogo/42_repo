ce97e5fb9b1104f439e61e3c9872cf09ffcd90b5
Theo Freydiere <tfreydie@z2r5p8.42lyon.fr>
2024-03-08 18:39:48 +0100

    le beau taff

--- solong/includes/so_long.h
+++ solong/includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 11:19:34 by tfreydie          #+#    #+#             */
-/*   Updated: 2024/03/07 15:18:12 by tfreydie         ###   ########.fr       */
+/*   Updated: 2024/03/08 18:36:07 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,32 @@
 # include <stdbool.h>
 # include "../libft/includes/libft.h"
 
+
+typedef struct s_xy
+{
+	int	x;
+	int	y;
+} t_xy;
+
 typedef struct	s_game 
 {
-    void    *wall;
-    void    *coins;
-    void    *floor;
-    void    *hero;
-    void    *exit;
-	int     height;
-    int     width;
-    char    *map;
-    int     map_height;
-    int     map_width;
+	void	*wall;
+	void	*coin;
+	void	*floor;
+	void	*hero;
+	void	*exit;
+	char	**map;
+	int		img_height;
+	int		img_width;
+	int		map_height;
+	int		map_width;
+	void	*mlx_win;
+	void	*mlx;
+	t_xy	player_xy;
 }   t_game;
 
+# define UP 	1
+# define DOWN 	2
+# define LEFT 	3
+# define RIGHT 	4
 #endif
--- solong/sources/so_long.c
+++ solong/sources/so_long.c
@@ -6,7 +6,7 @@
 /*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 11:19:05 by tfreydie          #+#    #+#             */
-/*   Updated: 2024/03/07 15:53:41 by tfreydie         ###   ########.fr       */
+/*   Updated: 2024/03/08 18:39:01 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 #include "so_long.h"
 #include "mlx_int.h"
 #include "mlx.h"
-
-
+#include <X11/X.h>
+#include <X11/keysym.h>
 
 char	*empty_malloc(void);
 void	perror_and_exit(char *error_message);
@@ -23,73 +23,211 @@ void	free_two_and_exit(void *tofree_1, void *tofree_2, char *error_message);
 char	*get_map_array(char *mapname, t_game *game);
 void	generate_map(t_game *game, void *mlx_win, void *mlx);
 void	init_all(t_game *game, char **argv, void *mlx);
-// int		key_hook(int keycode, t_vars *vars);
+int		exit_game(int keycode, t_game *game);
+void	move_player(t_game *game, void	*mlx_win, void	*mlx, int keycode);
+void	execute_move(t_game *game, void	*mlx_win, void	*mlx, t_xy to);
+void	move_player(t_game *game, void	*mlx_win, void	*mlx, int direction);
+int		is_valid_square(t_game *game, int direction);
+void	swap_char(char *hero, char *square);
+int		is_valid_move(int keycode);
+int		click_cross(t_game *game);
+
 
 int	main(int argc, char *argv[])
 {
-	void	*mlx_win;
-	void	*mlx;
 	t_game	game;
 
 	if (argc != 2)
 	    perror_and_exit("unvalid number of arguments\n");
-	mlx = mlx_init();
+	game.mlx = mlx_init();
 	// if (!mlx_win)
 	// 	perror_and_exit("failed to connect to graphical system\n");
-	init_all(&game, argv, mlx);
-	mlx_win = mlx_new_window(mlx, game.map_width * game.width, game.map_height * game.height, "Hello world!"); // to define dynamically in initall
-	if (!mlx_win)
+	init_all(&game, argv, game.mlx);
+	game.mlx_win = mlx_new_window(game.mlx, game.map_width * game.img_width, game.map_height * game.img_height, "Hello world!"); // to define dynamically in initall
+	if (!game.mlx_win)
 		perror_and_exit("window failed to create\n"); // need to free stuff
-	generate_map(&game, mlx_win, mlx);
-	// key_hook(keycode, mlx, mlx_win);
-	// mlx_key_hook()
-	mlx_loop(mlx);
-	free(game.map);
+
+	generate_map(&game, game.mlx_win, game.mlx);
+	//mlx_key_hook (game.mlx_win, exit_game, (void *)&game);
+	mlx_hook(game.mlx_win, KeyPress, KeyPressMask, exit_game, (void *)&game);
+	mlx_hook(game.mlx_win, DestroyNotify, StructureNotifyMask, click_cross, (void *)&game);
+	mlx_loop(game.mlx);
+	// free(game.map);
+	
+	//PROTOCOL TO FREE
+	//ON DETRUIT TOUTE LES IMAGES
+	// DETRUIT WINDOW
+	// DETRUIT MLX ?
+	// FREE MES MALLOCS
 }
-// int	key_hook(int keycode, t_vars *vars)
-// {
-// 	printf("Hello from key_hook!\n");
-// 	return (0);
-// }
+int	click_cross(t_game *game)
+{
+	mlx_destroy_image (game->mlx, game->coin);
+	mlx_destroy_image (game->mlx, game->wall);
+	mlx_destroy_image (game->mlx, game->hero);
+	mlx_destroy_image (game->mlx, game->exit);
+	mlx_destroy_image (game->mlx, game->floor);
+	mlx_destroy_window(game->mlx, game->mlx_win);
+	mlx_destroy_display(game->mlx);
+	ft_free_array((void **)game->map);
+	exit(EXIT_SUCCESS);
+}
+int	exit_game(int keycode, t_game *game)
+{
+	if (keycode == XK_Escape)
+	{	
+		mlx_destroy_image (game->mlx, game->coin);
+		mlx_destroy_image (game->mlx, game->wall);
+		mlx_destroy_image (game->mlx, game->hero);
+		mlx_destroy_image (game->mlx, game->exit);
+		mlx_destroy_image (game->mlx, game->floor);
+		mlx_destroy_window(game->mlx, game->mlx_win);
+		mlx_destroy_display(game->mlx);
+		ft_free_array((void **)game->map);
+		exit(EXIT_SUCCESS);
+	}
+	printf("hero x = %i y = %i target square x = %i y = %i \n", 
+	game->player_xy.x,
+	game->player_xy.y,
+	game->player_xy.x,
+	(game->player_xy.y - 1)
+	);
+	
+	if (is_valid_move(keycode))
+	{
+		char	*hero;
+		char	*target;
+		int		x;
+		int		y;
+		
+		int i = 0;
+		int j = 0;
+		x = game->player_xy.x;
+		y = game->player_xy.y;
+		hero = &(game->map[y][x]);
+		printf("game map variable = %c\n", game->map[y][x]);
+		printf("hero variable = %c\n", *hero);
+		if (keycode == XK_w || keycode == XK_W)
+			y--;
+		else if (keycode == XK_s || keycode == XK_S)
+			y++;
+		else if (keycode == XK_a || keycode == XK_A)
+			x--;
+		else if (keycode == XK_d || keycode == XK_D)
+			x++;
+		target = &(game->map[y][x]);
+		if ((*target) != '1' && (*target) != 'E')
+		{
+			swap_char(hero, target);
+			game->player_xy.x = x;
+			game->player_xy.y = y;
+			generate_map(game, game->mlx_win, game->mlx);
+		}
+		while (game->map[i])
+		{
+			j = 0;
+			while (game->map[i][j])
+			{
+				write(2, &game->map[i][j], 1);
+				j++;
+			}
+			write(2, "\n", 1);
+			i++;	
+		}
+	}
+	return (0);
+}
+void	swap_char(char *hero, char *square)
+{
+
+	*square = *hero;
+	*hero = '0';
+	return ;
+}
+
+int	is_valid_move(int keycode)
+{
+	static int valid_moves_keycode[] = {XK_a, XK_A, XK_w, XK_W, XK_s, XK_S, XK_d, XK_D, 0};
+	int	i;
+
+	i = 0;
+	while (valid_moves_keycode[i] != 0)
+	{
+		if (keycode == valid_moves_keycode[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
 void	generate_map(t_game *game, void *mlx_win, void *mlx)
 {
 	int	x;
 	int	y;
-	int	i;
 	
-	x = 0;
 	y = 0;
-	i = 0;
-	while (game->map[i])
+	// ////DEBUG////
+	// write(2, "THIS IS GENERATE MAP MAP\n", 25);
+	// int i = 0;
+	// int j = 0;
+	// while (game->map[i])
+	// {
+	// 	j = 0;
+	// 	while (game->map[i][j])
+	// 	{
+	// 		write(2, &game->map[i][j], 1);
+	// 		j++;
+	// 	}
+	// 	write(2, "\n", 1);
+	// 	i++;	
+	// }
+	// ///DEBUG///
+	while (game->map[y])
 	{
-		if (game->map[i] == '1')
-			mlx_put_image_to_window(mlx, mlx_win, game->wall, game->width * x, game->height * y);
-		if (game->map[i] == '0')
-			mlx_put_image_to_window(mlx, mlx_win, game->floor, game->width * x, game->height * y);
-		else if (game->map[i] == 'C')
-			mlx_put_image_to_window(mlx, mlx_win, game->coins, game->width * x, game->height * y);
-		else if (game->map[i] == 'E')
-			mlx_put_image_to_window(mlx, mlx_win, game->exit, game->width * x, game->height * y);
-		else if (game->map[i] == 'P')
-			mlx_put_image_to_window(mlx, mlx_win, game->hero, game->width * x, game->height * y);
-		else if (game->map[i] == '\n')
-		{	
-			y++;
-			x = -1;
+		x = 0;
+		while (game->map[y][x])
+		{
+			if (game->map[y][x] == '1')
+				mlx_put_image_to_window(mlx, mlx_win, game->wall, game->img_width * x, game->img_height * y);
+			if (game->map[y][x] == '0')
+				mlx_put_image_to_window(mlx, mlx_win, game->floor, game->img_width * x, game->img_height * y);
+			else if (game->map[y][x] == 'C')
+				mlx_put_image_to_window(mlx, mlx_win, game->coin, game->img_width * x, game->img_height * y);
+			else if (game->map[y][x] == 'E')
+				mlx_put_image_to_window(mlx, mlx_win, game->exit, game->img_width * x, game->img_height * y);
+			else if (game->map[y][x] == 'P')
+			{	
+				mlx_put_image_to_window(mlx, mlx_win, game->hero, game->img_width * x, game->img_height * y);
+				game->player_xy.x = x;
+				game->player_xy.y = y;
+			}
+			x++;
 		}
-		i++;
-		x++;
+		y++;
 	}
 	return ;
 }
+
 void	init_all(t_game *game, char **argv, void *mlx)
 {
-	game->map = get_map_array(argv[1], game);
-	game->wall = mlx_xpm_file_to_image(mlx, "./wall.xpm", &(game->width), &(game->height));
-    game->coins = mlx_xpm_file_to_image(mlx, "./coins.xpm", &(game->width), &(game->height));
-    game->floor = mlx_xpm_file_to_image(mlx, "./floor.xpm", &(game->width), &(game->height));
-    game->hero = mlx_xpm_file_to_image(mlx, "./hero.xpm", &(game->width), &(game->height));
-    game->exit = mlx_xpm_file_to_image(mlx, "./end.xpm", &(game->width), &(game->height));
+	char *map_array;
+	
+	
+	/////REDO THIS///
+	map_array = get_map_array(argv[1], game);
+	if (!map_array)
+		exit (EXIT_FAILURE);
+	game->map = ft_split(map_array, '\n');
+	if (!game->map)
+		exit (EXIT_FAILURE); // TODO CHANGE, AND SEE IF "" breaks the code;
+	free(map_array);
+	/////
+	game->wall = mlx_xpm_file_to_image(mlx, "./wall.xpm", &(game->img_width), &(game->img_height));
+    //check if fail AND check if it exists before destroy
+	game->coin = mlx_xpm_file_to_image(mlx, "./coin.xpm", &(game->img_width), &(game->img_height));
+	game->floor = mlx_xpm_file_to_image(mlx, "./floor.xpm", &(game->img_width), &(game->img_height));
+	game->hero = mlx_xpm_file_to_image(mlx, "./hero.xpm", &(game->img_width), &(game->img_height));
+    game->exit = mlx_xpm_file_to_image(mlx, "./end.xpm", &(game->img_width), &(game->img_height));
 }
 
 char	*get_map_array(char *mapname, t_game *game)
