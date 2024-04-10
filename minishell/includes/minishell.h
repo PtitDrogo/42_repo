11219c00814bc2bbfcb9ed7c7487d0efb7fd8173
Theo Freydiere <tfreydie@z2r5p8.42lyon.fr>
2024-04-10 18:14:24 +0200

    works more or less, save before trying to implement de garbage collector

---
+++ minishell/includes/minishell.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/19 16:14:17 by tfreydie          #+#    #+#             */
+/*   Updated: 2024/04/10 17:39:57 by tfreydie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+///------------------------Includes------------------------///
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h> 
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+
+///------------------------Structs------------------------///
+
+typedef struct s_env_node {
+
+	struct s_env_node	*next;
+	char				*variable;
+} t_env_node;
+
+typedef struct s_garbage_collect
+{
+	void						*to_free;
+	struct s_garbage_collect	*next;
+	
+} t_garbage_collect;
+
+///------------------------Functions------------------------///
+
+//garbage collector
+int		add_to_trash(t_garbage_collect **root, void *to_free);
+void    *malloc_trash(int size, t_garbage_collect *gc);
+int 	empty_trash(t_garbage_collect *gc);
+
+///------------------------Libft------------------------///
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *src);
+size_t	ft_strlen(const char *s);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
--- minishell/sources/export.c
+++ minishell/sources/export.c
@@ -6,45 +6,128 @@
 /*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 16:11:59 by tfreydie          #+#    #+#             */
-/*   Updated: 2024/04/09 16:15:48 by tfreydie         ###   ########.fr       */
+/*   Updated: 2024/04/10 18:13:58 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <stdio.h>
 
-int	find_n_destroy_env_variable(char **envp, char *env_to_find);
 
+int	unset(t_env_node *env_dup_root, char *env_to_find);
+int	export(t_env_node **root, void *variable);
+int	pop(t_env_node *env_dup_root, t_env_node *node_to_pop);
+
+//trying to access a freed pointer no bueno
 
 int main(int argc, char const *argv[], char **envp)
 {
-    t_env_node env_dup_root;
-    
+    t_env_node *env_dup_root;
+	t_env_node *to_free; // need to introduce my garbage collector later
+    int	i;
+	
+	i = -1;
+	env_dup_root = NULL;
+	printf("coucou\n");
+	if (envp == NULL)
+		return (1); //Error handling somehow
+	while (envp[++i]) // add condition if export fails
+		export(&env_dup_root, (void *)envp[i]);
+	//test
+	unset(env_dup_root, "DISPLAY=");
+	unset(env_dup_root, "IdontExist");
+	export(&env_dup_root, "MIAOU=VALGRIND");
+	export(&env_dup_root, "MIAOU2=VALGRIND2");
+	export(&env_dup_root, "MIAOU3=VALGRIND3");
+	unset(env_dup_root, "MIAOU2=");
+	//end test
+	
+	// this loop does memory freeing but it shouldnt after garbage c.
+	while (env_dup_root)
+	{
+		printf("%s\n", env_dup_root->variable);
+		free(env_dup_root->variable);
+		to_free = env_dup_root;
+		env_dup_root = env_dup_root->next;
+		free(to_free);
+	}
+	return (0);
 }
 
-
-int	find_n_destroy_env_variable(char **envp, char *env_to_find)
+int	unset(t_env_node *env_dup_root, char *env_to_find)
 {
-	int	i;
 	int	len_env;
-
-	if (!env_to_find || !envp || !envp[0]) // uh ?
+	t_env_node *current;
+	
+	current = env_dup_root;
+	if (!env_to_find || !env_dup_root) // uh ?
 	{
 		perror("Environnement does not exist");
 		exit(EXIT_FAILURE);
 	}
 	len_env = ft_strlen(env_to_find);
-	i = 0;
-	while (envp[i])
+	while (current)
 	{
-		if (ft_strnstr(envp[i], env_to_find, len_env))
+		if (ft_strnstr(current->variable, env_to_find, len_env))
 		{	
-			printf("I found the variable\n");
-			free(envp[i]);
-			envp[i] = NULL;
+			printf("\n\nI found the variable\n\n");
+			pop(env_dup_root, current);
 			return (1); // here we return the whole string;
 		}
-		i++;
+		current = current->next;
 	}
 	return (0); // couldnt find env variable
 }
+
+int	pop(t_env_node *env_dup_root, t_env_node *node_to_pop)
+{	
+	if (!env_dup_root || !node_to_pop)
+		return (0); //gotta check later;
+	while (env_dup_root->next && env_dup_root->next != node_to_pop)	
+		env_dup_root = env_dup_root->next;
+	if (env_dup_root->next == NULL)
+		return (0); // we couldnt find the node to pop
+	printf("\n\n im in poppast the checks\n\n");
+	env_dup_root->next = env_dup_root->next->next;
+	free(node_to_pop->variable); // MAYBE NOT NEEDED if we stick to the garbage collector
+	node_to_pop->variable = NULL;
+	free(node_to_pop);
+	return (1);
+}
+
+int	export(t_env_node **root, void *variable)
+{
+	//NEED TO ADD CONDITION SO I DONT EXPORT THE SAME NAME TWICE
+	
+	t_env_node	*new_node;
+	t_env_node	*current;
+
+	new_node = malloc(sizeof(t_env_node));
+	if (!new_node)
+		return (0);
+	new_node->next = NULL;
+	new_node->variable = ft_strdup(variable); // need to modify ft_strdup somehow;
+	if ((*root) == NULL)
+	{
+		*root = new_node;
+		return (1);
+	}
+	current = *root;
+	while (current->next)
+		current = current->next;
+	current->next = new_node;
+	return (1);
+}
+
+// int	check_if_variable_exit(t_env_node *root, void *variable)
+// {
+// 	//this needs to check if the variable already exist, and if 
+// 	//so, replace the content after the =;
+	
+// 	while (root)
+// 	{
+// 		if ()
+		
+// 		root = root->next;
+// 	}
+	
+// }
