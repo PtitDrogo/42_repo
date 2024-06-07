613b1b5a7c95c23986682560c5330f64fdc4cb62
ptitdrogo <moitheo2@hotmail.fr>
2024-06-08 01:13:07 +0200

    cpp template

---
+++ cpp/ex01/sources/phonebook.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ptitdrogo <ptitdrogo@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/08 00:55:24 by ptitdrogo         #+#    #+#             */
+/*   Updated: 2024/06/08 00:55:33 by ptitdrogo        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+class phonebook
+{
+private:
+    /* data */
+public:
+    phonebook(/* args */);
+    ~phonebook();
+};
+
+phonebook::phonebook(/* args */)
+{
+}
+
+phonebook::~phonebook()
+{
+}
