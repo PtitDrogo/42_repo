65463f17f42f7d78afe3e27e029ce10eba5961f4
Theo Freydiere <tfreydie@z2r10p6.42lyon.fr>
2024-06-14 17:06:34 +0200

    Ca print le message deux fois apres search WTF ??

--- cpp/ex01/includes/PhoneBook.h
+++ cpp/ex01/includes/PhoneBook.h
@@ -6,7 +6,7 @@
 /*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 12:43:05 by tfreydie          #+#    #+#             */
-/*   Updated: 2024/06/10 13:23:10 by tfreydie         ###   ########.fr       */
+/*   Updated: 2024/06/14 16:01:25 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,39 +14,57 @@
 #ifndef PHONEBOOK_H
 # define PHONEBOOK_H
 
-# define PRINT(x) std::cout << x << std::endl
-
-
+//----------------------INCLUDES----------------------//
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <sstream>
+#include <cctype>
 
+//----------------------DEFINES----------------------//
+# define PRINT(x) std::cout << x << std::endl
+# define PRINTN(x) std::cout << x
+# define FORMATPRINT(x) std::cout << std::right << std::setw(10) << std::setfill(' ') <<  x.substr(0, 10)
+
+
+
+//----------------------CLASSES----------------------//
 class Contact
 {
 private:
+    
+
+public:
     std::string first_name;
     std::string last_name;
     std::string nickname;
     std::string phone_number; 
     std::string darkest_secret;
-    // int age;
+    std::string age;
+    std::string index; //contact knows where it is
     
 
-public:
-
+Contact()
+        : 
+        first_name(""),
+        last_name(""),
+        nickname(""),
+        phone_number(""),
+        darkest_secret(""), 
+        index("") {}
 };
 
 class PhoneBook
 {
 private:
-    Contact list_of_contacts[8];
-    
     
 public:
-    void exit_phonebook(bool& keep_running);
-    void add();
-    void search();
+    int     cur_num_of_contacts;
+    Contact list_of_contacts[8];
+    PhoneBook() : cur_num_of_contacts(0) {}
+    void exitPhonebook(bool& keep_running);
+    void add(Contact *contact_list, int index);
+    void search(Contact contact_list[8]);
 };
-
-
 
 #endif
--- cpp/ex01/sources/main.cpp
+++ cpp/ex01/sources/main.cpp
@@ -6,27 +6,46 @@
 /*   By: tfreydie <tfreydie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 00:55:24 by ptitdrogo         #+#    #+#             */
-/*   Updated: 2024/06/10 13:23:37 by tfreydie         ###   ########.fr       */
+/*   Updated: 2024/06/14 16:59:45 by tfreydie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PhoneBook.h"
+static  void update_cur_index(int &index);
 
 int main(void)
 {
     PhoneBook   phonebook;
     bool        is_running = true;
     std::string input;
+    int         index = 0;
+    
     
     while (is_running)
     {
         PRINT("Hello, Phonebook here, type your command");
         getline(std::cin, input);
         if (input == "EXIT") {
-            phonebook.exit_phonebook(is_running);
+            phonebook.exitPhonebook(is_running);
+        }
+        if (input == "ADD") {
+            phonebook.add(phonebook.list_of_contacts, index);
+            update_cur_index(index);
+        }
+        if (input == "SEARCH") {
+            phonebook.search(phonebook.list_of_contacts);
+        }
+        if (!std::cin) {
+            phonebook.exitPhonebook(is_running);
         }
     }
-    
     return 0;
+}
+
+static  void update_cur_index(int &index) {
+    if (index == 7)
+        index = 0;
+    else
+        index++;
 }
 
