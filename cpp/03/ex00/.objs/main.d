fc2a55b6c10674ec78f928984316dfdc04fc4d66
Theo Freydiere <tfreydie@z1r4p1.42lyon.fr>
2024-07-18 06:00:04 +0200

    ClapTrap up and running

---
+++ cpp/03/ex00/.objs/main.d
@@ -0,0 +1,3 @@
+.objs/main.o: sources/main.cpp includes/ClapTrap.h
+
+includes/ClapTrap.h:
---
+++ cpp/03/ex01/includes/ClapTrap.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+class ClapTrap
+{
+private:
+    std::string _name;
+    int _hit_points;
+    int _energy_points;
+    int _attack_damage;
+public:
+    //Constructors
+    ClapTrap();
+    ClapTrap(ClapTrap& other);
+    ClapTrap(std::string _name);
+    
+    //Destructors
+    ~ClapTrap();
+
+    //Overloads
+    ClapTrap& operator=(const ClapTrap& other);
+
+    //Getters
+    std::string get_name() const;
+    int get_hp() const;
+    int get_energy() const;
+    int get_attack() const;
+
+    //Method
+    void attack(const std::string& target);
+    void takeDamage(unsigned int amount);
+    void beRepaired(unsigned int amount);
+};
+
+std::ostream    &operator<<(std::ostream &o, ClapTrap const &fixed);
