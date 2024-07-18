5454812437a9573d5542e3a4cdb27b6d32d23953
Theo Freydiere <tfreydie@z1r4p1.42lyon.fr>
2024-07-18 02:46:18 +0200

    fixed ex06

--- cpp/01/ex06/.objs/Harl.d
+++
@@ -1,5 +0,0 @@
-.objs/Harl.o: sources/Harl.cpp includes/main.h includes/Harl.h
-
-includes/main.h:
-
-includes/Harl.h:
--- cpp/01/ex06/.objs/main.d
+++
@@ -1,5 +0,0 @@
-.objs/main.o: sources/main.cpp includes/main.h includes/Harl.h
-
-includes/main.h:
-
-includes/Harl.h:
