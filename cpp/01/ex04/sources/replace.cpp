1c4ebda424f79fda5dde0111a5cad4994bd04bc7
Theo Freydiere <tfreydie@z3r4p2.42lyon.fr>
2024-08-25 14:31:11 +0200

    fixed sed

--- cpp/01/ex04/sources/replace.cpp
+++ cpp/01/ex04/sources/replace.cpp
@@ -8,29 +8,37 @@ void Replace::find_and_replace(char const *file_name, char const *s1, char const
     this->_outfile_name = this->_filename + ".replace";
     
     std::ifstream infile(this->_filename.c_str());
-    std::ofstream outFile(this->_outfile_name.c_str(), std::ios::trunc);
     std::string cur_line;
     std::string substring;
 
-    while (std::getline(infile, cur_line))
+    if (_to_find.empty())
     {
-        size_t pos = cur_line.find(this->_to_find);
-        std::size_t start = 0;
-        
-        if (pos != std::string::npos) 
+        std::cerr << "please type a not empty char to replace" << std::endl;
+        return ;
+    }
+    if (infile.is_open())
+    {
+        std::ofstream outFile(this->_outfile_name.c_str(), std::ios::trunc);
+        while (std::getline(infile, cur_line))
         {
-            while (pos != std::string::npos)
+            size_t pos = cur_line.find(this->_to_find);
+            std::size_t start = 0;
+            
+            if (pos != std::string::npos) 
             {
-                substring = cur_line.substr(start, pos - start);
-                outFile << substring << this->_to_replace;
-                start = pos + this->_to_find.size();
-                pos = cur_line.find(this->_to_find, start);
+                while (pos != std::string::npos)
+                {
+                    substring = cur_line.substr(start, pos - start);
+                    outFile << substring << this->_to_replace;
+                    start = pos + this->_to_find.size();
+                    pos = cur_line.find(this->_to_find, start);
+                }
+                outFile << cur_line.substr(start, pos) << std::endl;
             }
-            outFile << cur_line.substr(start, pos) << std::endl;
-        }
-        else
-        {
-            outFile << cur_line << std::endl;
+            else
+            {
+                outFile << cur_line << std::endl;
+            }
         }
     }
 
